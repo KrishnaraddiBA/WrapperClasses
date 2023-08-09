c2dd820f637166c7505bf5a1b8d00a3c4b9d3854
Krishnaraddi <krishnaraddi.alagawadi1997@gmail.com>
2023-08-09 14:53:29 +0530

    first

---
+++ A.java
@@ -0,0 +1,14 @@
+package WrapperClass;
+
+public class A {
+	
+	public static void main(String[] args) {
+		Integer i=0;
+		//there are many wrapper classes like Integer,Float,Byte,Short,Chartacter,
+		//these wrapper class will be having many inbuilt methods .
+		System.out.println(i.MIN_VALUE);
+		System.out.println(i.MAX_VALUE);
+		System.out.println(i.hashCode(25));
+	}
+
+}
